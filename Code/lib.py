9c6d6f274c7db25b7a7a30aa5ae8f75fb1796022
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-06-17 16:16:00 -0500

    change background color add spaces for readability

--- Code/lib.py
+++ Code/lib.py
@@ -9,10 +9,10 @@ def drawPage( folder ):
   df = pd.read_csv( folder + "/data.csv", index_col=0 )
   with open( folder + "/text.txt", "r") as myfile:
       lines = myfile.readlines()
-  plt.subplots(2, 1)
-  ax = plt.subplot(2, 1, 1)
+  plt.subplots( 2, 1, facecolor=background_color )
+  ax = plt.subplot( 2, 1, 1 )
   drawText( ax, lines )
-  ax = plt.subplot(2, 1, 2)
+  ax = plt.subplot( 2, 1, 2 )
   drawStacks( ax, df )
 
 def drawText( ax, lines ):
@@ -21,7 +21,7 @@ def drawText( ax, lines ):
           , color = 'k'
           , fontproperties = font_light
           , verticalalignment="center" )
-  ax.axis('off')
+  ax.axis( 'off' )
 
 def drawStacks( ax, df ):
   nCols = len( df.columns )
@@ -30,8 +30,8 @@ def drawStacks( ax, df ):
 
   if True: # draw stuff
     plots = {}
-    for rn in range(nRows):
-      if rn < 1: bottom = [0. for i in range(nCols)]
+    for rn in range( nRows ):
+      if rn < 1: bottom = [0. for i in range( nCols )]
       else:      bottom = df.iloc[0:rn,:].sum()
       top =      bottom + df.iloc[  rn,:]
       plots[rn] = ax.bar(
@@ -43,9 +43,9 @@ def drawStacks( ax, df ):
         ax.text( float( cn )
                , ((bottom + top) / 2)[cn]
                , df.iloc[ rn, cn ]
-               , fontsize=10
-               , verticalalignment='center'
-               , horizontalalignment='center'
+               , fontsize = 10
+               , verticalalignment = 'center'
+               , horizontalalignment = 'center'
                , color = 'w'
                , fontproperties = font_light )
 
@@ -58,16 +58,17 @@ def drawStacks( ax, df ):
     leg = plt.legend( plots.values()
                     , df.index
                     , prop = font_light
+                    , facecolor = background_color
                     , shadow=True
             # Next arguments: draw the legend to the right of the plot, ala
             # https://pythonspot.com/matplotlib-legend/
-                    , loc='upper center'
-                    , bbox_to_anchor=(1.45, 0.8)
-                    , ncol=1 )
+                    , loc = 'upper center'
+                    , bbox_to_anchor = ( 1.45, 0.8 )
+                    , ncol = 1 )
     plt.setp( leg.get_texts()
-            , color='k' )
+            , color = 'k' )
 
-    del(bottom, plots)
+    del( bottom, chartBox, leg, plots )
 
   if True: # add labels
     # Vertical axis needs a label, but no ticks, and no tick labels. Based on
@@ -83,13 +84,15 @@ def drawStacks( ax, df ):
                  , fontproperties = font_light )
 
     plt.xticks( xvals, df.columns )
-    plt.setp( ax.get_xticklabels() ,visible=True
+    plt.setp( ax.get_xticklabels()
+            , visible = True
             , color = 'k'
             , fontproperties = font_light )
-    plt.setp( ax.get_yticklabels(), visible=False )
+    plt.setp( ax.get_yticklabels(), visible = False )
     ax.tick_params( axis='x', which='both', length=0 )
     ax.tick_params( axis='y', which='both', length=0 )
     ax.set_frame_on(False)
 
+background_color = "xkcd:salmon"
 font_black = fm.FontProperties( fname = "fonts/Montserrat_Black.ttf" )
 font_light = fm.FontProperties( fname = "fonts/Montserrat_Light.ttf" )
