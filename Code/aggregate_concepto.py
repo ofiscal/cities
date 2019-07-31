594f3b8abc8b9224b26b0c6ab11c6040728b8050
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-07-30 19:05:01 -0500

    When matching first n subcodes,   exclude codes for which there are exactly n.

--- Code/aggregate_concepto.py
+++ Code/aggregate_concepto.py
@@ -1,3 +1,7 @@
+# Herein a "subcode" is defined as a substring of maximal
+# length with no periods in it. For instance, in "ab.cd",
+# "ab" is a subcode, but not "a".
+
 from typing import List
 import numpy as np
 import pandas as pd
@@ -8,14 +12,18 @@ import re
 no_period_regex : re.Pattern = (
   re.compile( "[^\.]" ) )
 
-def n_code_regex( n : int ) -> re.Pattern:
-  preRegex = "[^\.]+\.?"
+def regex_for_more_than_n_codes( n : int ) -> re.Pattern:
+  """ If a code had exactly n subcodes,
+  the last would have no trailing period. """
+  preRegex = "[^\.]+\."
   return re.compile(
     "".join( ["^"] +
              [ preRegex for _ in range(0,n) ] ) )
 
-def first_n_subcodes( n : int, code : str ) -> List[int]:
-  matches = re.findall( n_code_regex(n)
+def first_n_proper_subcodes( n : int, code : str ) -> List[int]:
+  """ Finds the first n subcodes of a code,
+  but only if it has *more* than n of them. """
+  matches = re.findall( regex_for_more_than_n_codes(n)
                       , code )
   if not matches:
     return np.nan
@@ -25,6 +33,7 @@ def first_n_subcodes( n : int, code : str ) -> List[int]:
                          , match )
     return subcodes
 
-assert( first_n_subcodes( 2, "a.b.c" ) == ["a","b"] )
-assert( first_n_subcodes( 2, "a.b" ) == ["a","b"] )
-assert( pd.isnull( first_n_subcodes( 2, "a" ) ) )
+assert ["a","b"] == first_n_proper_subcodes( 2, "a.b.c.d" )
+assert ["a","b"] == first_n_proper_subcodes( 2, "a.b.c"   )
+assert pd.isnull(   first_n_proper_subcodes( 2, "a.b" ) )
+assert pd.isnull(   first_n_proper_subcodes( 2, "a"   ) )
