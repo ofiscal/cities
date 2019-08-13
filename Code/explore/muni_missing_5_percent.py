2edb3677acd719c0f7058ddd835a4d474932c11c
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-08-13 16:33:27 -0500

    bughunt in progress: missing muni codes   discover where the number missing goes from ~4% to 100%   muni_missing_5_percent.py: generalize to multiple data sources     previously it only examined the raw data

--- Code/explore/muni_missing_5_percent.py
+++ Code/explore/muni_missing_5_percent.py
@@ -7,31 +7,45 @@ import Code.build.aggregation_regexes as ac
 import Code.build.sisfut_metadata as sm
 
 
-######
-###### Build "concepto" data
-######
-# These are the data sets "ingresos", "inversion", and "funcionamiento",
-# collecting across years and dropping verbose duplicative columns.
+def get_raw_data():
+  dfs = {}
+  for series in sm.series:
+    dfs[series] = pd.DataFrame()
+    for year in range( 2012, 2018+1 ):
+      shuttle = (
+        pd.read_csv(
+          ( sm.source_folder + "original_csv/"
+            + str(year) + "_" + series + ".csv" )
+  #        , nrows = 20000
+          , usecols = [ "Cód. DANE Municipio",
+                        "Nombre DANE Municipio" ] ) .
+        rename( columns = dict( sm.column_subsets[series] ) ) )
+      shuttle["year"] = year
+      dfs[series] = dfs[series] . append(shuttle)
+  return dfs
 
-dfs = {}
-for series in sm.series:
-  dfs[series] = pd.DataFrame()
-  for year in range( 2012, 2018+1 ):
-    shuttle = (
-      pd.read_csv(
-        ( sm.source_folder + "original_csv/"
-          + str(year) + "_" + series + ".csv" )
-#        , nrows = 20000
-        , usecols = [ "Cód. DANE Municipio",
-                      "Nombre DANE Municipio" ] ) .
-      rename( columns = dict( sm.column_subsets[series] ) ) )
-    shuttle["year"] = year
-    dfs[series] = dfs[series] . append(shuttle)
+def get_output_data(folder):
+  dfs = {}
+  for series in sm.series:
+    dfs[series] = pd.read_csv( folder + "/" + series + ".csv" )
+  return dfs
 
-for s in sm.series:
-  print(s)
-  df = dfs[s]
-  df_bad = df[
-    ( df["muni code"] .isnull() ) &
-    ( df["muni"]      .isnull() ) ]
-  print( len(df_bad) / len(df) )
+def analyze( dfs ):
+  for s in sm.series:
+    print(s)
+    df = dfs[s]
+    df_bad = df[
+      ( df["muni code"] .isnull() ) |
+      ( True if not "muni" in df.columns
+        else df["muni"]      .isnull() ) ]
+    print( len(df_bad) / len(df) )
+
+dfs = get_raw_data()
+analyze( dfs )
+
+dfs1 = get_output_data("output/conceptos_1")
+analyze( dfs1 )
+
+dfs2 = get_output_data("output/conceptos_2/recip-1")
+analyze( dfs )
+
