c66ea25692c6526dfde5bd77a619a3ad838f24df
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-08-15 18:56:40 -0500

    workflow improvement   Test the length of sys.argv to determine whether using interpreter or command line.   No more need to edit an import (or set of them) when using the other.

--- Code/common.py
+++ Code/common.py
@@ -1,4 +1,11 @@
-import Code.params.fixed as imp # for when using the interpreter
-# import Code.params.cl as imp    # for when using the command line
+from sys import argv
+
+# If argv > 1, we are using the command line.
+# Otherwise, we are in the interpreter.
+
+if len( argv) > 1:
+  import Code.params.cl    as imp
+else:
+  import Code.params.fixed as imp
 
 subsample = imp.subsample
