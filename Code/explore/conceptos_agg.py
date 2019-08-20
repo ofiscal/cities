0abc06810e64b81d6245c5e3d3b212101a039b61
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-08-20 16:01:08 -0500

    minor - incorporate downstream column name changes

--- Code/explore/conceptos_agg.py
+++ Code/explore/conceptos_agg.py
@@ -1,20 +1,21 @@
 # To determine whether the (muni,year,concepto)-aggregated data makes sense.
 
 # import Code.explore.conceptos_agg as eca
-from itertools import chain
 import numpy as np
 import pandas as pd
-import Code.util as util
-import Code.build.aggregation_regexes as ac
+
+import Code.common as c
 import Code.build.sisfut_metadata as sm
 
 
 # columns defining the unit of observation
-columns_uob = ["muni","year","codigo","codigo-top"]
+columns_uob = ["muni code","year","item categ","item top"]
 
 dfs = {}
 for s in sm.series:
-  dfs[s] = pd.read_csv( "output/conceptos_3_agg/" + s + ".csv" )
+  dfs[s] = pd.read_csv(
+    "output/conceptos_3_agg/recip-" + str(c.subsample) +
+    "/" + s + ".csv" )
 
 ing = dfs["ingresos"]
 inv = dfs["inversion"]
@@ -25,26 +26,25 @@ def summarize_col( col : str, df : pd.DataFrame ) -> pd.DataFrame:
   theMin = df[col].min()
   theMax = df[col].max()
   theRatio = 0 if theRange == 0 else theRange / theMin
-  return pd.DataFrame( { ( col + "range") : [theRange]
-                       , ( col + "min"  ) : [theMin]
-                       , ( col + "max"  ) : [theMax]
-                       , ( col + "ratio") : [theRatio] } )
+  return pd.DataFrame( { ( col + ", range") : [theRange]
+                       , ( col + ", min"  ) : [theMin]
+                       , ( col + ", max"  ) : [theMax]
+                       , ( col + ", ratio") : [theRatio] } )
 
 df = ing
 x = ( df .
-  groupby( by = ["muni","year","codigo"] ) .
+  groupby( by = ["muni code","year","item categ"] ) .
   apply( lambda df :
-         summarize_col( "Presupuesto Definitivo",
-                        df) ) )
-#   reset_index() .
-#   drop( columns = ["level_3"] ) )
+         summarize_col( "item def", df) ) .
+   reset_index() .
+   drop( columns = ["level_3"] ) )
 
-df["Presupuesto Inicial"].max()
-df["Presupuesto Inicial"].min()
-x["ratio"].min()
-x["ratio"].max()
-x[ x["ratio"] == np.inf ]
-len( x[ x["max"] < 0.5 ] )
-x[ x["max"] > 1 ].min()
+df["item def"].max()
+df["item def"].min()
+x["item def, ratio"].min()
+x["item def, ratio"].max()
+x[ x["item def, ratio"] == np.inf ]
+len( x[ x["item def, max"] < 0.5 ] )
+x[ x["item def, max"] > 1 ].min()
 
 # ingresos -- looks good. max ratio (of presupuestal inicial)  is infinity, but those are 0.01 pesos / 0 pesos
