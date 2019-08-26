83fb33768d6a42caff6198922b954f8782b13f23
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-08-26 18:08:17 -0500

    draw each of the three kinds of series

--- Code/main.py
+++ Code/main.py
@@ -4,37 +4,35 @@ import matplotlib.pyplot as plt
 from   matplotlib.backends.backend_pdf import PdfPages
 
 import Code.common as c
-import Code.build.sisfut_metadata as sm
+import Code.series_metadata as ser
 import Code.draw.lib as lib
 
 
 source = "output/conceptos_4_top_categs_only_and_scaled/recip-" + str(c.subsample)
 
 dfs = {}
-for s in sm.series:
-  df = pd.read_csv( source + "/" + s + ".csv" )
-  dfs[s] = df
+for s in ser.series:
+  df = pd.read_csv( source + "/" + s.name + ".csv" )
+  dfs[s.name] = df
 
-for s in ["ingresos"]:
+for s in ser.series:
+  print( s.name, s.pesos_col )
   for muni in [15764]:
-    df = dfs[s]
+    df = dfs[s.name]
     df_muni = ( df
                 [ df["muni code"] == muni ]
-                [["year","item", "item recaudo"]] )
+                [["year","item", s.pesos_col]] )
     df_pivot = (
       df_muni .
       pivot( index = "item",
              columns = "year",
-             values = "item recaudo" ) .
-      astype(int) ) # TODO : this should happen upstream
-    if True: # This is to make df_pivot resemble the example.
-      # It doesn't seem to help.
-      df_pivot.index.name = None
-      df_pivot.columns.name = None
+             values = s.pesos_col ) .
+      fillna( 0 ) )
+    df_pivot = df_pivot.astype(int) # TODO : this should happen upstream
     dest = 'output/reports/' + str(muni)
     if not os.path.exists( dest ):
       os.makedirs( dest )
-    with PdfPages( dest + "/" + s + ".pdf" ) as pdf:
+    with PdfPages( dest + "/" + s.name + ".pdf" ) as pdf:
       lib.drawPage( df_pivot, ["Title?"], ["Text?"] )
       pdf.savefig( facecolor=lib.background_color )
       plt.close()
---
+++ Code/series_metadata.py
@@ -0,0 +1,21 @@
+# There are three series for each municipality:
+# ingresos, inversion and funcionamiento.
+# Each is drawn a little differently; this file specifies how.
+
+
+class seriesType:
+  def __init__(self, name, pesos_col):
+    self.name = name
+    self.pesos_col = pesos_col
+
+ingresos       = seriesType(
+  name = "ingresos",
+  pesos_col = "item recaudo" )
+inversion      = seriesType(
+  name = "inversion",
+  pesos_col = "item oblig" )
+funcionamiento = seriesType(
+  name = "funcionamiento",
+  pesos_col = "item oblig" )
+
+series = [ingresos,inversion,funcionamiento]
