4e5bff7a1ae1da852c0f1a86994a2b3bbac816e6
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-08-28 17:18:32 -0500

    clean inflation series

---
+++ Code/build/inflation.py
@@ -0,0 +1,24 @@
+# The inflation series is indexed by a composite "year-month"
+# string column. We only need the 12th month of each year after 2011.
+
+import pandas as pd
+
+
+def is_last_month(s : str) -> bool:
+  return s[-2:] == "12"
+def is_after_2011(s : str) -> bool:
+  return int( s[:4] ) > 2011
+def year_month_to_year(s : str) -> int:
+  return int( s[:4] )
+
+deflator = pd.read_csv("data/inflation/raw.csv",encoding="utf-16")
+deflator = ( deflator
+             [ deflator["when"] .
+               apply( is_last_month ) ] )
+deflator = ( deflator
+             [ deflator["when"] .
+               apply( is_after_2011 ) ] )
+deflator["when"] = ( deflator["when"] .
+                     apply( year_month_to_year ) )
+deflator.to_csv( "data/inflation/final.csv",
+                 index = False )
