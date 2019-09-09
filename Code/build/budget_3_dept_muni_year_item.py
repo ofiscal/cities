ca3ad2c687141aadfd523aa574807481cc36fd6b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-09-09 15:25:36 -0500

    tax_categ_subtract()   handle missing values

--- Code/build/budget_3_dept_muni_year_item.py
+++ Code/build/budget_3_dept_muni_year_item.py
@@ -5,8 +5,8 @@
 #   because rows with a missing group variable disappear
 #   upon using pandas.DataFrame.groupby().
 #
-# Why: For most of the data, a given (muni, year, item) triple
-#   identifies exactly one row, but sometimes not,
+# Why aggregate: For most of the data, a given (muni, year, item)
+#   triple identifies exactly one row -- but sometimes it does not,
 #   because that kind of spending is divided by fuente and ejecutor,
 #   as demonstrated in explore/duplicate_rows.py.
 
@@ -19,6 +19,7 @@ if True:
   import Code.build.classify_budget_codes as codes
   import Code.build.sisfut_metadata as sm
 
+
 if True:
   budget_key = pd.read_csv( "output/keys/budget.csv",
                             encoding = "utf-16" )
@@ -26,8 +27,8 @@ if True:
   dest   = "output/budget_3_dept_muni_year_item/recip-" + str(c.subsample)
   if not os.path.exists( dest ):
     os.makedirs(         dest )
-  dfs0, dfs1 = {}, {} # input and output
-  group_vars = ["muni code","dept code","year","item categ"]
+  dfs0, dfs1, dfs2 = {}, {}, {} # input, midput, output
+  spacetime = ["muni code","dept code","year"]
 
 
 ######
@@ -37,7 +38,8 @@ if True:
 for s in ["ingresos","gastos"]:
   dfs0[s] = pd.read_csv( source + "/" + s + ".csv",
                         encoding = "utf-16" )
-  df = dfs0[s]
+  dfs1[s] = dfs0[s].copy()
+  df = dfs1[s]
   if True: # manip indiv columns
     df["muni code"] = ( df["muni code"] .
                         fillna(-1) )
@@ -47,21 +49,20 @@ for s in ["ingresos","gastos"]:
       apply( lambda c: codes.codes_to_categs[c] ) )
     df = df.drop( columns = ["item code"] )
   df = util.to_front( # aggregate within item categories
-    group_vars,
+    spacetime + ["item categ"],
     ( df .
-      groupby( by = group_vars ) .
+      groupby( by = spacetime + ["item categ"] ) .
       agg( sum ) .
       reset_index() .
-      sort_values( group_vars ) ) )
-  dfs1[s] = df
+      sort_values( spacetime + ["item categ"] ) ) )
 
 if True: # for ingresos data, subtract transfers from recursos propios
-  def tax_categ_fix (
+  def tax_categ_subtract (
       subtract : "x",      # the categ value of rows to subtract
       subtract_from : "x", # the categ value of rows to subtract from
       categ : str, # the name of an "x"-valued column
-      value : str, # a peso-valued column
-      df0 : pd.DataFrame
+      value : str, # the name of a peso-valued column
+      df0 : pd.DataFrame # a single muni-dept-year cell
       ) -> pd.DataFrame:
     """ In a given (muni,dept,year) frame,
     the peso-valued column in the row for which categ =
@@ -72,6 +73,7 @@ if True: # for ingresos data, subtract transfers from recursos propios
     df = df0.copy()
     subtract_vec = (df[ df[categ] == subtract ]
                     [value] )
+    if len(subtract_vec) < 1: return df
     assert len(subtract_vec) == 1
     df.loc[ df[categ] == subtract_from,
             value ] = (
@@ -82,11 +84,30 @@ if True: # for ingresos data, subtract transfers from recursos propios
   if True: # test it
     x = pd.DataFrame( { "cat" : ["1", "2", "3"],
                         "val" : [ 1,   2,   3 ] } )
-    assert ( tax_categ_fix( "1", "2", "cat", "val", x ) .
+    assert ( tax_categ_subtract( "1", "2", "cat", "val", x ) .
+             # subtract val at row where cat=1 from val at row where cat=2
              equals(
                pd.DataFrame( { "cat" : ["1", "2", "3"],
                                "val" : [ 1.0, 1.0, 3.0 ] } ) ) )
-  assert "TODO: resume here" == False
+    assert ( tax_categ_subtract( "0", "2", "cat", "val", x ) .
+             # here the subtract code ("0") is not present
+             equals( x ) )
+
+dfs2 ["gastos"] = dfs1["gastos"] # pointer equality is fine here
+dfs2 ["ingresos"] = (
+  dfs1 ["ingresos"] . copy() .
+  groupby( spacetime ) .
+  apply( lambda df :
+         tax_categ_subtract(
+           subtract = "TI.A.2.6",
+           subtract_from = "TI.A",
+           categ = "item categ",
+           value = "item recaudo",
+           df0 = df ) ) .
+  reset_index() )
+
+assert "TODO: resume here" == False
+
 
 ######
 ###### Test, output
