fe22a3f634033861abf7a13c805d29496a4c98da
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-09-09 16:50:34 -0500

    Test the daylights out of tax_categ_subtract().   It works in all tests, even on subsets of the real data,   but still not when applied to the full data set.

--- Code/build/budget_3_dept_muni_year_item.py
+++ Code/build/budget_3_dept_muni_year_item.py
@@ -35,9 +35,15 @@ if True:
 ###### Build
 ######
 
+# dfs0: read data
 for s in ["ingresos","gastos"]:
   dfs0[s] = pd.read_csv( source + "/" + s + ".csv",
                         encoding = "utf-16" )
+
+# dfs1: fill NaN values in muni with -1,
+# create "item categ" and drop "item code",
+# aggregate within spacetime-categ cells
+for s in ["ingresos","gastos"]:
   dfs1[s] = dfs0[s].copy()
   df = dfs1[s]
   if True: # manip indiv columns
@@ -53,10 +59,12 @@ for s in ["ingresos","gastos"]:
     ( df .
       groupby( by = spacetime + ["item categ"] ) .
       agg( sum ) .
-      reset_index() .
-      sort_values( spacetime + ["item categ"] ) ) )
+      reset_index() ) )
 
-if True: # for ingresos data, subtract transfers from recursos propios
+# dfs2: For ingresos only, for each spacetime slice,
+# subtract the "recaudo" value in the row where categ = "transfers"
+# from the "recaudo" value in the row where categ = "propios".
+if True:
   def tax_categ_subtract (
       subtract : "x",      # the categ value of rows to subtract
       subtract_from : "x", # the categ value of rows to subtract from
@@ -64,57 +72,73 @@ if True: # for ingresos data, subtract transfers from recursos propios
       value : str, # the name of a peso-valued column
       df0 : pd.DataFrame # a single muni-dept-year cell
       ) -> pd.DataFrame:
-    """ In a given (muni,dept,year) frame,
-    the peso-valued column in the row for which categ =
-    recursos propios is too big.
-    From it we must subtract the value in the peso-valued column
-    in the row where categ = transferencias.
-    This function generalizes that problem. """
     df = df0.copy()
     subtract_vec = (df[ df[categ] == subtract ]
                     [value] )
     if len(subtract_vec) < 1: return df
     assert len(subtract_vec) == 1
+    if True: # debugging
+      print( "\nOld df:\n", df )
+      print( "subtract from: ",
+             df.loc[ df[categ] == subtract_from, value ] )
+      print( "subtract: ", subtract_vec )
+      print( "result: ", (
+        df.loc[ df[categ] == subtract_from,
+                value ] -
+        float( subtract_vec ) ) )
     df.loc[ df[categ] == subtract_from,
             value ] = (
       df.loc[ df[categ] == subtract_from,
               value ] -
       float( subtract_vec ) )
+    print( "New df:\n", df )
     return df
-  if True: # test it
-    x = pd.DataFrame( { "cat" : ["1", "2", "3"],
-                        "val" : [ 1,   2,   3 ] } )
+  if True: # test it on fake data
+    x = pd.DataFrame( { "cat" : [ "1", "2", "3"],
+                        "val" : [ 11,  12,  13 ] } )
     assert ( tax_categ_subtract( "1", "2", "cat", "val", x ) .
              # subtract val at row where cat=1 from val at row where cat=2
              equals(
-               pd.DataFrame( { "cat" : ["1", "2", "3"],
-                               "val" : [ 1.0, 1.0, 3.0 ] } ) ) )
+               pd.DataFrame( { "cat" : ["1",  "2", "3"],
+                               "val" : [ 11.0, 1.0, 13.0 ] } ) ) )
     assert ( tax_categ_subtract( "0", "2", "cat", "val", x ) .
              # here the subtract code ("0") is not present
              equals( x ) )
 
-dfs2 ["gastos"] = dfs1["gastos"] # pointer equality is fine here
-dfs2 ["ingresos"] = (
-  dfs1 ["ingresos"] . copy() .
-  groupby( spacetime ) .
-  apply( lambda df :
-         tax_categ_subtract(
-           subtract = "TI.A.2.6",
-           subtract_from = "TI.A",
-           categ = "item categ",
-           value = "item recaudo",
-           df0 = df ) ) .
-  reset_index() )
+if True: # test it on one spacetime slice of the real data
+  s = "ingresos"
+  spot = dfs1[s][spacetime].iloc[0]
+  df1 = dfs1[s][ (dfs1[s][spacetime] == spot).transpose().all() ]
+  df2 = tax_categ_subtract(
+    subtract = "Por transferencias de la Nación",
+    subtract_from = "Por recursos propios",
+    categ = "item categ",
+    value = "item recaudo",
+    df0 = df1 )
+  assert ( (df1["item recaudo"] > df2["item recaudo"]) .
+           any() )
+  assert (         df1.drop(columns = ["item recaudo"] ) .
+           equals( df2.drop(columns = ["item recaudo"] ) ) )
+  # del( s, spot, df1, df2 )
 
-assert "TODO: resume here" == False
+if True: # do it to all the (applicable) real data
+  dfs2 ["gastos"] = dfs1["gastos"] # pointer equality is fine here
+  dfs2 ["ingresos"] = (
+    dfs1 ["ingresos"] . copy() .
+    groupby( spacetime ) .
+    apply( lambda df :
+           tax_categ_subtract(
+             subtract = "Por transferencias de la Nación",
+             subtract_from = "Por recursos propios",
+             categ = "item categ",
+             value = "item recaudo",
+             df0 = df ) ) .
+    reset_index( drop=True ) )
 
 
 ######
 ###### Test, output
 ######
-
-for s in ["ingresos","gastos"]:
-  print( len(dfs0[s]), len(dfs1[s]) )
 
 for s in ["ingresos","gastos"]:
   df.to_csv( dest + "/" + s + ".csv" ,
