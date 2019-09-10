8020080936ce1c5d2ff585e8ba4d851abd312caf
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-09-10 15:26:12 -0500

    toward integration-testing the sample tables

--- Code/sample_tables_test.py
+++ Code/sample_tables_test.py
@@ -7,65 +7,36 @@ if True:
   import Code.sample_tables_defs as defs
 
 
-######
-###### Unit test
-######
+if True: # Get raw 2018 data
+  common = { "Nombre DANE Departamento" : "dept",
+             "Nombre DANE Municipio" : "muni",
+             "Código Concepto" : "item" }
+  raw_2018 = "data/sisfut/original_csv/2018_"
+  #  
+  def geo_select( df : pd.DataFrame ) -> pd.DataFrame:
+    return pd.concat(
+      [ df[ df["muni"].isin( { "BOGOTÁ, D.C.",
+                               "SANTA MARTA",
+                               "FILANDIA",
+                               "VALLE DEL GUAMUEZ" } ) ],
+        df[   pd.isnull(df["muni"] ) &
+            ( df["dept"].isin( [ "ANTIOQUIA",
+                                 "CESAR",
+                                 "CHOCÓ",
+                                 "ARAUCA" ] ) ) ] ],
+      axis = "rows" )
+  #
+  def get_2018( filename: str,
+                money_column: str
+              ) -> pd.DataFrame:
+    return geo_select(
+      pd.read_csv( raw_2018 + filename + ".csv",
+                   usecols = list(common.keys()) + [money_column] ) .
+      rename( columns = dict( common, **{money_column:"money"} ) ) )
+  ing = get_2018( "ingresos", "Recaudo" )
+  inv = get_2018( "inversion", "Obligaciones" )
+  fun = get_2018( "funcionamiento", "Obligaciones" )
 
-realish_data = (
-  # Real-ish in the sense that there are multiple group columns,
-  # and a string variable that is meaningless when summed.
-  pd.DataFrame( {
-    "where" : [5,5,5,5,           6,6,6,6,          5,5,5,5],
-    "when"  : [1,1,1,1,           1,1,1,1,          2,2,2,2],
-    "item"  : ["0","1","2","3",  "0","1","2","3",  "0","1","2","3"],
-    "value" : [0,1,2,3,           10,11,12,13,      0,-1,-2,-3] } ) )
 
-assert (
-  defs .
-  sum_all_but_greatest_n_rows_in_groups(
-    2, ["where","when"], ["value"], ["item"], realish_data )
-  [["where","when","item","value"]] .
-  reset_index( drop = True ) .
-  equals( pd.DataFrame(
-    { "where" : [5,5,5,           5,5,5,            6,6,6],
-      "when"  : [1,1,1,           2,2,2,            1,1,1],
-      "item"  : [np.nan,"2","3",  np.nan,"1","0",  "2","3",np.nan],
-      "value" : [1,2,3,          -5,-1,0,           12,13,21] } ) ) )
-
-
-######
-###### Integration test
-######
-
-if True: # read input data
-  dis = {}
-  for s in ser.series:
-    dis[s.name] = pd.read_csv(
-      ( "output/budget_7_verbose/recip-" + str(c.subsample)
-        + "/" + s.name + ".csv"),
-      encoding = "utf-16" )
-
-if True: # read output data
-  dos = {}
-  for s in ser.series:
-    dos[s.name] = pd.read_csv(
-      ( "output/sample_tables/recip-" + str(c.subsample)
-        + "/" + s.name + ".csv"),
-      encoding = "utf-16" )
-
-group_vars = ["dept","muni","year"]
-for n in ["gastos"]:
-  di,do = dis[n], dos[n]
-
-spot = di.iloc[-1][group_vars]
-def at_spot( spot, df ):
-  return df[ (df[group_vars] == spot).transpose().all() ]
-
-### RESUME HERE: Problem: The "other" column is a sum that includes
-### the broken out rows as well as the ones I intended to include.
-bogota_in = ( at_spot( spot, di )
-              [ group_vars + ["item code","item oblig"]] .
-              sort_values( group_vars + ["item oblig"] ) )
-at_spot( spot, do )[ group_vars + ["item code","item oblig"]]
-bogota_in["item oblig"].sum()
-
+if True:
+  assert "Resume here" == False
