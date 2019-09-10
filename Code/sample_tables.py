b9ce0d7dfc8ec68dcfa7ef79356079af3778363c
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-09-10 14:53:16 -0500

    sample_tables.py:   use only year 2018   draw no pivot tables     move that code to TODO.org

--- Code/sample_tables.py
+++ Code/sample_tables.py
@@ -8,10 +8,10 @@ if True:
   import Code.sample_tables_defs as defs
   import Code.series_metadata as ser
 
-
-group_vars = ["dept", "muni", "year"]
-
-assert c.subsample == 1 # This program expects the full sample.
+if True:
+  group_vars = ["dept", "muni", "year"]
+  geo_vars   = ["dept", "muni", "dept code", "muni code"]
+  assert c.subsample == 1 # This program expects the full sample.
 
 if True: # read data
   raw = {}
@@ -23,74 +23,47 @@ if True: # read data
         encoding = "utf-16" ) .
       sort_values( group_vars ) )
 
-if True: # restrict to the munis and depts we need,
-         # and sort within group by budget item value
-  geo_sample = {}
+if True: # restrict to the spacetime we need
+  spacetime_sample = {}
   for s in ser.series:
     df = raw[s.name]
-    geo_sample[s.name] = (
-      pd.concat(
-        [ df[ df["muni"].isin( { "BOGOTÁ, D.C.",
-                                 "SANTA MARTA",
-                                 "FILANDIA",
-                                 "VALLE DEL GUAMUEZ" } ) ],
-          df[ ( df["muni code"] == -1 ) &
-              ( df["dept"].isin( [ "ANTIOQUIA",
-                                   "CESAR",
-                                   "CHOCÓ",
-                                   "ARAUCA" ] ) ) ] ],
-        axis = "rows" ) .
-      sort_values( group_vars + [s.pesos_col] ) )
+    df = pd.concat(
+      [ df[ df["muni"].isin( { "BOGOTÁ, D.C.",
+                               "SANTA MARTA",
+                               "FILANDIA",
+                               "VALLE DEL GUAMUEZ" } ) ],
+        df[ ( df["muni code"] == -1 ) &
+            ( df["dept"].isin( [ "ANTIOQUIA",
+                                 "CESAR",
+                                 "CHOCÓ",
+                                 "ARAUCA" ] ) ) ] ],
+      axis = "rows" )
+    df = ( df[ df["year"] == 2018 ] .
+           drop( columns = "year" ) )
+    spacetime_sample[s.name] = df
 
 if True: # group all but the biggest five categories
   items_grouped = {}
   for s in ser.series:
-    df = geo_sample[s.name]
-    items_grouped[s.name] = util.to_front(
-      group_vars + ["item categ", s.pesos_col],
+    df = spacetime_sample[s.name]
+    df = util.to_front(
+      ["muni" ,"dept", "item categ", s.pesos_col],
       defs.sum_all_but_greatest_n_rows_in_groups(
         n = 5,
-        group_vars = group_vars,
+        group_vars = geo_vars,
         sort_vars = [s.pesos_col],
-        meaningless_to_sum = ["dept code","muni code","item categ"],
+        meaningless_to_sum = ["item categ"],
         df0 = df ) )
-    if True: # output one big table
-      dest = "output/sample_tables/recip-" + str(c.subsample)
-      if not os.path.exists( dest ):
-        os.makedirs(         dest )
-      items_grouped[s.name].to_csv(
-        dest + "/" + s.name + ".csv",
-        encoding = "utf-16",
-        index = False )
+    df["item categ"].fillna("other")
+    items_grouped[s.name] = df
 
-def output_pivot( df0 : pd.DataFrame,
-                  dest_root : str ):
-  df = df0.reset_index() # to restore muni code and dept code
-  if True: # get our bearings
-    muni_code = str( df["muni code"].iloc[0] )
-    dept_code = str( df["dept code"].iloc[0] )
-    muni      = str( df["muni"].iloc[0] )
-    dept      = str( df["dept"].iloc[0] )
-    dest = dest_root + "/" + dept_code + "_" + muni_code
-    if not os.path.exists( dest ):
-      os.makedirs(         dest )
-  if True: # output a readme that says where this is, and a pivot table
-    ( df.pivot( index = "item categ",
-                columns = "year",
-                values = s.pesos_col ) .
-      to_csv( dest + "/data.csv", # in this case we *do* want the index
-              encoding = "utf-16" ) )
-    with open( dest + "/README.txt", "w" ) as f:
-      f.write( "Dept: " + dept + "\n" +
-               "Muni: " + muni + "\n" )
-
-if True: # write many little pivot tables, one for each place:
-         # its columns are years, and its rows item categs
+if True: # output two big tables
+  dest = "output/sample_tables/recip-" + str(c.subsample)
+  if not os.path.exists( dest ):
+    os.makedirs(         dest )
   for s in ser.series:
-    df = items_grouped[s.name].copy()
-    ( df . groupby( ["dept code","muni code"] ) .
-      apply( lambda df:
-             output_pivot(
-               df0       = df,
-               dest_root = "output/sample_pivots" ) ) )
+    items_grouped[s.name].to_csv(
+      dest + "/" + s.name + ".csv",
+      encoding = "utf-16",
+      index = False )
 
--- Code/sample_tables_defs.py
+++ Code/sample_tables_defs.py
@@ -3,14 +3,14 @@ if True:
   import numpy as np
   import pandas as pd
 
-test_data = (
+test_data = ( # for safety, these names are similar but intentionally not
+              # identical to the ones used in production
   pd.DataFrame( {
-    "year"      : [1,1,1,1,2,2,2,2],
-    "year+1"    : [2,2,2,2,3,3,3,3], # ala the (dept,dept code) redundancy
-    "item code" : [0,1,2,3,0,1,2,3],
-    "value"     : [0,1,2,3,
-                   10,11,12,13] } ) .
-  sort_values( ["year","value"] ) )
+    "yr"    : [1,1,1,1,  2,2,2,2],
+    "yr+1"  : [2,2,2,2,  3,3,3,3], # ala the (dept,dept code) redundancy
+    "categ" : [0,1,2,3,  0,1,2,3],
+    "money" : [0,1,2,3,  10,11,12,13] } ) .
+  sort_values( ["yr","money"] ) )
 
 def last_n_in_groups(
     n : int,
@@ -28,12 +28,12 @@ def last_n_in_groups(
 
 if True: # test it
   assert (
-    last_n_in_groups( 2, ["year"], test_data ) .
+    last_n_in_groups( 2, ["yr"], test_data ) .
     equals(
-      pd.DataFrame( {"year"      : [1,1,2,2],
-                     "year+1"    : [2,2,3,3],
-                     "item code" : [2,3,2,3],
-                     "value"     : [2,3,12,13] } ) ) )
+      pd.DataFrame( {"yr"    : [1, 1,  2,  2],
+                     "yr+1"  : [2, 2,  3,  3],
+                     "categ" : [2, 3,  2,  3],
+                     "money" : [2, 3, 12, 13] } ) ) )
 
 def sum_of_all_but_last_n_rows_in_groups(
     n : int,
@@ -56,19 +56,19 @@ if True: # test it
   assert (
     sum_of_all_but_last_n_rows_in_groups(
       n = 2,
-      group_vars =["year","year+1"],
+      group_vars = ["yr","yr+1"],
       df0 = test_data ) .
-    drop( columns = "item code" ) .
-    equals( pd.DataFrame( { "year"   : [1,2],
-                            "year+1" : [2,3],
-                            "value"  : [1,21] } ) ) )
+    drop( columns = "categ" ) . # meaningless when summed
+    equals( pd.DataFrame( { "yr"    : [1,2],
+                            "yr+1"  : [2,3],
+                            "money" : [1,21] } ) ) )
 
 def sum_all_but_greatest_n_rows_in_groups(
-    n : int,
-    group_vars : List[str],
-    sort_vars : List[str],
+    n                  : int,
+    group_vars         : List[str],
+    sort_vars          : List[str],
     meaningless_to_sum : List[str],
-    df0 : pd.DataFrame
+    df0                : pd.DataFrame
     ) -> pd.DataFrame:
   """ PITFALL: Has a very similar name to a function it uses.
   This is only function intended to face out of this module;
@@ -77,7 +77,7 @@ def sum_all_but_greatest_n_rows_in_groups(
          sort_values( group_vars + sort_vars ) )
   indiv = last_n_in_groups( n, group_vars, df )
   grouped = sum_of_all_but_last_n_rows_in_groups(
-    n, group_vars, df )
+      n, group_vars, df )
   grouped = grouped.drop(
     columns = set.intersection( # TODO : why do I need this hack?
       # Dropping meaningless_to_sum seems like it should be enough;
@@ -93,14 +93,15 @@ def sum_all_but_greatest_n_rows_in_groups(
 if True: # test it
   assert (
     sum_all_but_greatest_n_rows_in_groups(
-      2, ["year","year+1"], ["value"], ["item code"], test_data ) .
+      2, ["yr","yr+1"], ["money"], ["categ"], test_data ) .
     reset_index( drop = True ) .
     equals (
-      pd.DataFrame( { "item code" : [2,3,np.nan,
-                                     2,3,np.nan],
-                      "value"     : [2,3,1,
-                                    12,13,21],
-                      "year"      : [1,1,1,
-                                     2,2,2],
-                      "year+1"    : [2,2,2,
-                                     3,3,3] } ) ) )
+      pd.DataFrame( { "categ"  : [2,3,np.nan,
+                                  2,3,np.nan],
+                      "money"  : [2,3,1,
+                                12,13,21],
+                      "yr"     : [1,1,1,
+                                  2,2,2],
+                      "yr+1"   : [2,2,2,
+                                  3,3,3] } ) ) )
+
