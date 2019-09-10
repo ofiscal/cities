e63d211ad5537d394f38dd7b8fdbaa13df673931
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-09-09 19:30:26 -0500

    delete another mention of "item"   this time outside of Code/build/

--- Code/sample_tables.py
+++ Code/sample_tables.py
@@ -13,10 +13,12 @@ group_vars = ["dept", "muni", "year"]
 if True: # read data
   raw = {}
   for s in ser.series:
-    raw[s.name] = pd.read_csv(
-      ( "output/budget_7_verbose/recip-" + str(c.subsample)
-        + "/" + s.name + ".csv"),
-      encoding = "utf-16" )
+    raw[s.name] = (
+      pd.read_csv(
+        ( "output/budget_7_verbose/recip-" + str(c.subsample)
+          + "/" + s.name + ".csv"),
+        encoding = "utf-16" ) .
+      sort_values( group_vars ) )
 
 if True: # restrict to the munis and depts we need,
          # and sort within group by budget item value
@@ -46,11 +48,10 @@ if True:
   for s in ser.series:
     df = geo_sample[s.name]
     items_grouped[s.name] = (
-      defs.sum_all_but_last_n_rows_in_groups(
-        5, group_vars, [s.pesos_col], ["item","item categ"], df ) )
+      defs.sum_all_but_greatest_n_rows_in_groups(
+        5, group_vars, [s.pesos_col], ["item categ"], df ) )
     items_grouped[s.name].to_csv(
       dest + "/" + s.name + ".csv",
       encoding = "utf-16",
       index = False )
-
 
