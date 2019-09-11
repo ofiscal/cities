6a3a1ebb7f1497436dd2c5a259ce6759a88754af
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-09-11 18:58:13 -0500

    compute department-level regalías ingresos

--- Code/build/budget_5_add_regalias.py
+++ Code/build/budget_5_add_regalias.py
@@ -2,35 +2,54 @@
 # It just copies the gastos data unchanged to a new directory --
 # CPU-inefficient, yes, but safer than doing something more confusing.
 
-import os
-import pandas as pd
+if True:
+  import os
+  import pandas as pd
+  #
+  import Code.common as c
+  import Code.series_metadata as ser
 
-import Code.common as c
-import Code.series_metadata as ser
+if True: # bearings
+  source = "output/budget_4_scaled/recip-"       + str(c.subsample)
+  dest   = "output/budget_5_add_regalias/recip-" + str(c.subsample)
+  if not os.path.exists( dest ):
+    os.makedirs(         dest )
 
+if True: # input
+  dfsi = {}
+  for s in ser.series:
+    dfsi[s.name] = pd.read_csv( source + "/" + s.name + ".csv",
+                               encoding = "utf-16" )
+  #
+  regalias = (
+    pd.read_csv( "output/regalias.csv",
+                 encoding="utf-16" ) .
+    rename( columns = { "regalias" :
+                        ser.series_dict["ingresos"].pesos_col } ) )
 
-source = "output/budget_4_scaled/recip-"       + str(c.subsample)
-dest   = "output/budget_5_add_regalias/recip-" + str(c.subsample)
-if not os.path.exists( dest ):
-  os.makedirs(         dest )
+if True: # sum muni-regalias to the dept level
+  dept_regalias = (
+    regalias . copy() .
+    groupby( ["dept code","year"] ) .
+    agg( sum ) .
+    reset_index() )
+  dept_regalias["muni code"] = -1
 
-dfs = {}
-for s in ser.series:
-  dfs[s.name] = pd.read_csv( source + "/" + s.name + ".csv",
-                             encoding = "utf-16" )
+if True:
+  regalias      ["item categ"] = "regalias"
+  dept_regalias ["item categ"] = "regalias"
 
-regalias = (
-  pd.read_csv( "output/regalias.csv",
-               encoding="utf-16" ) .
-  rename( columns = { "regalias" :
-                      ser.series_dict["ingresos"].pesos_col } ) )
-regalias["item categ"] = "regalías"
-
-dfs["ingresos"] = pd.DataFrame.append( dfs["ingresos"],
-                                       regalias,
-                                       sort = False )
-
-for s in ser.series:
-  dfs[s.name].to_csv( dest + "/" + s.name + ".csv",
-                      index = False,
-                      encoding = "utf-16" )
+if True: # output
+  dfso = {}
+  dfso["gastos"] = dfsi["gastos"]
+  dfso["ingresos"] = pd.concat(
+    [ dfsi["ingresos"],
+      regalias,
+      dept_regalias ],
+    sort = True, # sort columns by name so they align
+    axis = "rows" )
+  for s in ser.series:
+    dfso[s.name].to_csv(
+      dest + "/" + s.name + ".csv",
+      index = False,
+      encoding = "utf-16" )
