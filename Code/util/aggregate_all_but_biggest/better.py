b9ce9265c740285081d8268eb8a5d09f1f8d4089
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-07 10:45:35 -0500

    create the "top n" column   based on the "top five" column

--- Code/util/aggregate_all_but_biggest/better.py
+++ Code/util/aggregate_all_but_biggest/better.py
@@ -29,3 +29,28 @@ if True:
         { "money" : [1,2,3,11,22],
           "top five" : [0,0,0,1,1] } ) ) )
 
+if True:
+  def add_top_n_column(
+      df0 : pd.DataFrame
+      ) -> pd.DataFrame:
+    """ Expects a space slice with a "top five" column.
+    Creates a "top n" column, which is 1 for every item category
+    that is in the top five items for some year. """
+    df = df0.copy()
+    top_rows = df[ df["top five"] == 1 ]
+    top_items = set( top_rows["item categ"].unique() )
+    df["top n"] = ( df["item categ"] .
+                    apply( lambda cell: int( cell in top_items ) ) )
+    return df
+  if True: # Test it
+    td = pd.DataFrame( { "city"       : [1,1,1, 2,2,2],
+                         "item categ" : [1,2,3, 1,2,3],
+                         "top five"   : [0,0,1, 0,1,0] } )
+    top_n = pd.DataFrame( { "top n"   : [0,1,1, 0,1,1] } )
+      # In city 1, item categ 3 is top-five.
+      # In city 2, item categ 2 is.
+      # Therefore any row with item categ 2 or 3 is top-n.
+    assert ( add_top_n_column(td) .
+             equals( pd.concat( [td, top_n],
+                                axis = 'columns' ) ) )
+
