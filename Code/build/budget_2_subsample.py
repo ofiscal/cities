957fb14efbbbc9638ad8b207cf96502e7314788b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-08 17:10:24 -0500

    simplify the daylights out of subsampling

--- Code/build/budget_2_subsample.py
+++ Code/build/budget_2_subsample.py
@@ -13,61 +13,44 @@ if True:
   #
   import Code.build.budget_1p5_tests as test
   import Code.build.budget_2_subsample_defs as defs
+  import Code.build.use_keys as uk
   import Code.metadata.terms as t
-  import Code.metadata.two_series as ser
+  import Code.metadata.two_series as s2
 
 if not os.path.exists( defs.top_dest ):
   os.makedirs( defs.top_dest )
 
-# For the recip-1/ folder, use a symblink; don't copy the full sample.
+# For the recip-1/ folder, use a symlink; don't copy the full sample.
 if True:
   if os.path.exists(       defs.sub_dest(1) ):
     os.remove(             defs.sub_dest(1) )
   os.symlink( defs.source, defs.sub_dest(1) )
 
 if True:
-  dfs                = defs.read_data()
-  dfs_muni, dfs_dept = {},{}
-  for s in ser.series: # deep copy
-    sn = s.name
-    dfs_muni[sn] = dfs[sn] [ dfs[sn]["muni code"] != 0] . copy()
-    dfs_dept[sn] = dfs[sn] [ dfs[sn]["muni code"] == 0] . copy()
-    assert len(dfs[sn]) == len(dfs_muni[sn]) + len(dfs_dept[sn])
-    assert len( dfs_muni[sn].index .
-                intersection(
-                  dfs_dept[sn].index ) ) == 0
-    assert len(dfs_muni[sn]) > 0
-    assert len(dfs_dept[sn]) > 0
-  #
-  munis = defs.munis_unique_no_dept( dfs )
-  assert (len(munis) < 1150) & (len(munis) > 1050)
-  del(dfs)
+  dfs = defs.read_data()
+  munis_df = uk.geo[["dept code","muni code"]]
+  depts_df = uk.depts[["dept code"]]
+  depts_df["muni code"] = 0
 
-for subsample in [1000,100,10]: # smallest first, to catch errors faster
+for subsample in [1000,100,10]: #,100,10]: # smallest first, to catch errors faster
   if not os.path.exists( defs.sub_dest( subsample ) ):
     os.makedirs(         defs.sub_dest( subsample ) )
-  muni_code_subsample = defs.subsample( subsample,
-                                        munis )
-  dfs_muni_subset   = defs.dfs_subset( muni_code_subsample,
-                                       dfs_muni )
-  test.column_names_after_agg( [t.ingresos,t.gastos],
-                               dfs_muni_subset )
-  for s in [t.ingresos,t.gastos]:
-    # Test that the length of each subsample is reasonable.
-    # The 1/10 subsample, for instance, should be not less than half,
-    # and not more than twice, len(df) / 10.
-    # It's not exact because some cities have more budget items recorded than others.
-    df    = dfs_muni       [s]
-    dfsub = dfs_muni_subset[s]
-    assert ( ( 2 *   len(df) / subsample )
-             >=      len(dfsub) )
-    assert ( ( 0.5 * len(df) / subsample )
-             <=      len(dfsub) )
-  for s in [t.ingresos,t.gastos]:
-    m = dfs_muni_subset[s]
-    d = dfs_dept       [s]
-    assert len( m.index.intersection( d.index ) ) == 0
-    m . append(d) . to_csv(
-      defs.sub_dest( subsample ) + "/" + s + ".csv",
-      index = False )
+  if True: # build `places` : Set[ (dept code, muni code) ]
+    munis_df_subsample = defs.subsample( subsample,
+                                         munis_df )
+    depts_df_subsample = defs.subsample( subsample,
+                                         depts_df )
+    places = pd.concat(
+      [ munis_df_subsample,
+        depts_df_subsample,
+        pd.DataFrame( { "dept code" : [5],      # to ensure there's at least one
+                        "muni code" : [0] } ) ] ) # department-level observation
+  for s in s2.series:
+    df = dfs[s.name]
+    df = df.merge( places,
+                   on = ["dept code","muni code"],
+                   how = "inner" )
+    df . to_csv( ( defs.sub_dest( subsample ) +
+                   "/" + s.name + ".csv" ),
+                 index = False )
 
--- Code/build/budget_2_subsample_defs.py
+++ Code/build/budget_2_subsample_defs.py
@@ -26,33 +26,8 @@ def read_data( nrows = None ):
     dfs[filename] = df
   return dfs
 
-def munis_unique_no_dept( dfs : Dict[str, pd.DataFrame]
-                        ) -> pd.DataFrame:
-  """Creates a pandas DataFrame that contains every "muni code" value exactly once, omitting 0 (because 0 represents a dept, not a muni)."""
-  munis = pd.DataFrame()
-  for s in [t.ingresos,t.gastos]:
-    munis = pd.concat( [ munis,
-                         dfs[s] ["muni code"] ],
-                       axis = "rows" )
-  munis.columns = ["muni code"]
-  munis = munis[ munis["muni code"] != 0 ] # drop dept-level observations
-  return ( munis .
-           drop_duplicates() )
-
 def subsample( subsample : int,
                df : pd.DataFrame ) -> pd.DataFrame:
   return df.sample( frac = 1/subsample,
                     random_state = 0 ) # seed
-
-def dfs_subset( munis_subset, dfs ):
-  dfs2 = {}
-  for s in dfs.keys(): # deep copy
-    dfs2[s] = dfs[s].copy()
-  for filename in [t.ingresos,t.gastos]:
-    df = dfs2[filename]
-    df = df[ df["muni code"] .
-             isin( set(
-               munis_subset["muni code"] ) ) ]
-    dfs2[filename] = df
-  return dfs2
 
