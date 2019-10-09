94fa645588183c14b570b0f68ef68c1bde1fad98
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-08 20:39:58 -0500

    bugfix: include deuda in integration test for stage 1p5

--- Code/integ_tests/iBudget_1p5.py
+++ Code/integ_tests/iBudget_1p5.py
@@ -5,7 +5,7 @@ if True:
   import Code.build.classify_budget_codes as codes
   import Code.build.use_keys as uk
   import Code.integ_tests.integ_util as iu
-  import Code.metadata.two_series as ser
+  import Code.metadata.two_series as s2
 
 name_of_data_source = "budget_1p5"
 
@@ -13,7 +13,7 @@ s1p5_dfs = {} # stage 1.5 (build/budget_1p5) data frames
 for name in ["ingresos","gastos"]:
   s1p5_dfs[name] = uk.merge_geo(
     pd.read_csv(
-      "output/budget_1p5/" + name + ".csv" )
+      "output/budget_1p5/" + name + ".csv" ) )
 
 for s in ["ingresos","gastos"]:
   print( len(s1p5_dfs[s]) )
@@ -24,7 +24,8 @@ if True:
       ("ingresos",codes.of_interest["ingresos"]),
       ("gastos",set.union(
         codes.of_interest["inversion"],
-        codes.of_interest["funcionamiento"] ) ) ]:
+        codes.of_interest["funcionamiento"],
+        codes.of_interest["deuda"] ) ) ]:
     df = s1p5_dfs[name]
     smaller[name] = (
       df.copy()
@@ -35,10 +36,10 @@ if True:
             | (   ( pd.isnull( df["muni"] ) )
                 & (            df["dept"] == iu.dept ) ) ) ] )
     smaller[name]["muni"] = (
-      smaller[name]["muni"].fillna(-1) )
+      smaller[name]["muni"].fillna("dept") )
   if True:
     for (name,money_column) in [
-        ("ingresos","item recaudo"),
+        ("ingresos","item total"),
         ("gastos","item oblig") ]:
       print(
         "\DISAGGREGATED: " + name_of_data_source + ": " + name + "\n",
