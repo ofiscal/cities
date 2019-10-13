bc2f8b5cf8227afd9d7daf71e45f12532b4a7a1b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-12 22:29:11 -0500

    + def static_avg( filename : str, +                 money_col : str, +                 dept_code : int, +               ) -> pd.Series:

--- Code/build/budget_9_static.py
+++ Code/build/budget_9_static.py
@@ -10,51 +10,79 @@ if True:
   import pandas as pd
   #
   import Code.common as c
-  import Code.util.aggregate_all_but_biggest.better \
-    as agger # "aggregator"
+  import Code.metadata.terms as t
   import Code.metadata.four_series as s4
 
+testing = True
+
 if True:
-  grouped_root = "output/pivots/recip-" + str(c.subsample)
+  monolith_root = "output/budget_7_verbose/recip-" + str(c.subsample)
+  by_place_root = "output/pivots/recip-"           + str(c.subsample)
   spacetime = ["dept", "muni", "year", "dept code", "muni code"]
   space     = ["dept", "muni",         "dept code", "muni code"]
 
 if True: # read a few big tables
-  ungrouped : Dict[str, pd.DataFrame] = {}
-  for s in s4.series:
-    ungrouped[s.name] = (
-      pd.read_csv(
-        ( "output/budget_7_verbose/recip-" + str(c.subsample)
-          + "/" + s.name + ".csv") ) .
+  monolith : Dict[str, pd.DataFrame] = {}
+  for s in s4.series_pct:
+    monolith[s.name] = ( # These data sets are "monolithic" in that
+      # all munis are contained together in the same data set.
+      # By contrast, each output of this file corresponds to
+      # to a separate muni.
+      pd.read_csv( monolith_root + "/" + s.name + ".csv") .
       sort_values( spacetime ) )
 
-if True: # build geo indices of interest
-  geo = ungrouped[s.name] . copy()
-  geo = ( geo[space] .
-         groupby( space ) .
-         agg( 'first' ) .
-         reset_index() )
+geo = ( # geo indices of interest
+  monolith[ t.ingresos_pct] .
+    # smaller than, and (here) equivalent to, t.gastos_pct
+  copy()
+  [space] .
+  drop_duplicates() )
 
 def static_muni( filename : str,
                  dept_code : int,
                  muni_code : int
-               ) -> pd.DataFrame:
+               ) -> pd.Series:
+  assert muni_code > 0
   d = str( geo.loc[ geo["muni code"]==muni_code,
                     "dept" ] .
            iloc[0] )
-  m = ( str( geo.loc[ geo["muni code"]==muni_code,
-                      "muni" ] .
-             iloc[0] )
-        if muni_code > 0
-        else "promedio" )
-  df = pd.read_csv(
-    ( grouped_root + "/" + d + "/" + m +
-      "/" + filename + ".csv" ),
+  m = str( geo.loc[ geo["muni code"]==muni_code,
+                    "muni" ] .
+           iloc[0] )
+  fn = ( by_place_root + "/" + d + "/" + m +
+         "/" + filename + ".csv" )
+  df = pd.read_csv( fn,
     index_col="item categ" )
   df.columns = list( map( int,
                           map( float, df.columns ) ) )
   return ( df[[2016,2017,2018]] .
            sum( axis="columns" ) )
+
+if testing: # Test by hand
+  static_muni( "gastos-pct", 25, 25873 )
+
+def static_avg( filename : str,
+                money_col : str,
+                dept_code : int,
+              ) -> pd.Series:
+  df = monolith[filename] . copy()
+  df = df [ (df["dept code"]==dept_code) &
+            (df["muni code"] > 0) & # exclude dept-level info
+            (df["year"] >= 2016) ]
+  ms = df["munis in dept"].iloc[0]
+  df = df.drop( columns = "munis in dept" )
+  dg = ( df .
+         groupby( ["dept code","dept","item categ"] ) .
+         agg( 'sum' ) .
+         reset_index() )
+  res = ( dg[ money_col ] /
+          float(3 * ms) ) # three because there are three years
+  res.index = dg["item categ"]
+  return res
+
+if testing:
+  ( static_avg( "gastos-pct", "item oblig", 25 ) .
+    drop( columns = ["dept code","dept"] ) )
 
 def static_avg_with_otros(
     filename : str,
