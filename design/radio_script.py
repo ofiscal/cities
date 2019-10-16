cd86ca6ca2d640526734fa5679b0fac5bcfbd43b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-16 15:15:59 -0500

    documentation

---
+++ design/radio_script.py
@@ -0,0 +1,15 @@
+Píldora municipal para [[MUNICIPALITY NAME]],  [[DEPARTMENT]] (Código DANE: [[MUNICIPALITY DANE CODE]] ) 
+
+Recuerde: su voto determina en manos de quién van a quedar los recursos de [[MUNICIPALITY NAME]]. Por eso es importante que conozca de dónde viene la plata su municipio y cómo la gastan sus gobernantes. 
+
+Mi nombre es Luis Carlos Reyes y soy el director del Observatorio Fiscal de la Universidad Javeriana. Con el fin de que usted pueda conocer cómo se usan los recursos de su municipio, el Observatorio ha preparado un análisis de lo que pasa con este dinero, para que usted evalúe el uso que la alcaldía y el concejo municipal les dan a los recursos públicos.
+
+¿Sabía que el año pasado el gobierno de [[MUNICIPALITY NAME]] contó con recursos de $[[TOTAL MUNICIPALITY INCOME 2018]]? Este es el dinero con el cual se pueden financiar servicios como el transporte escolar de sus hijos,  la construcción de vías terciarias para llevar productos desde las fincas hasta el mercado, la atención médica y las adecuaciones a los hospitales del municipio. 
+
+Durante este gobierno, el [[MUNICIPALITY’S PERCENTAGE OF TRANSFERENCIAS DEL GOBIERNO NACIONAL INCOME 2015-2018]]% de los recursos del municipio vinieron de transferencias del Gobierno Nacional, y el [[MUNICIPALITY’S PERCENTAGE OF REGALÍAS INCOME 2015-2018]]% de las regalías que se obtuvieron por la explotación petrolera y minera. Si bien estos recursos no provienen de los impuestos pagados por los habitantes del municipio, son suyos y es responsabilidad del alcalde y el concejo administrarlos responsablemente y para el beneficio de todos.
+
+Además, alrededor de [[PERCENTAGE OF TAX INCOME 2015-2018, NO % SIGN]] de cada 100 pesos que le entraron a [[MUNICIPALITY NAME]] vienen de impuestos que pueden ser aumentados o reducidos por el alcalde y el Concejo, como el impuesto predial que se les puede cobrar a los dueños de fincas y casas, o el ICA que se les puede cobrar a los negocios. El concejo municipal y el alcalde pueden decidir si subir o bajar estos impuestos 
+
+En el Observatorio Fiscal de la Universidad Javeriana nuestra tarea es que tenga insumos suficientes para definir su voto, y apoyarlo con su tarea de conocer cómo se reparte la torta de los ingresos en su municipio. Así que aquí le contamos que el principal gasto de [[MUNICIPALITY NAME]] durante esta administración ha sido en salud, pues se ha invertido un [[MUNICIPALITY SHARE OF SPENDING IN HEALTH 2015-2018]] del presupuesto del municipio; se ha invertido el [[MUNICIPALITY SHARE OF SPENDING IN HOUSING 2015-2018]] en infraestructura y vivienda, y el [[MUNICIPALITY SHARE OF SPENDING IN EDUCATION 2015-2018]] en educación.
+
+Como ciudadano de [[MUNICIPALITY NAME]], usted conoce mejor que nadie si estos recursos se están empleando adecuadamente. ¿Qué promesas están haciendo los candidatos? ¿Sí alcanza la plata para lo que están prometiendo? ¿Qué gastos tendrían que recortar para financiar lo que prometen, o qué impuestos tendrían que subir? Su voto define cómo se manejarán los recursos de su municipio. Exíjales cuentas claras a los candidatos al concejo y a la alcaldía este 27 de octubre y, ante todo, vote.
