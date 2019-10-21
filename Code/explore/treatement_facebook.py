47e1b129a9d287efea45f35d84b49436faf376bb
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-21 17:04:21 -0500

    exploratory code for facebook treatment group

--- Code/explore/treatement_facebook.py
+++ Code/explore/treatement_facebook.py
@@ -20,6 +20,18 @@ treated = (
          on = "muni code" ) .
   merge( geo, how = "inner", on = "muni code" ) )
 
+( treated . # for the README file in the cities-output repo
+  apply(
+    ( lambda row:
+      "[" + row["dept"] + " -- " + row["muni"] +
+      # "](https://raw.githubusercontent.com/ofiscal/cities-output/master/reportes/" +
+     "](http://luiscarlosreyes.com/wp-content/uploads/2019/10/" +
+     str(row["muni code"]) + ".pdf)\n"
+     ),
+    axis = "columns" ) .
+  to_csv( "for-readme.txt",
+         index = False) )
+
 total_pop = treated["population"].sum()
 
 treated["pop fraction"] = (
@@ -32,18 +44,27 @@ treated["budget"] = (
 
 treated["muni short"] = (
   treated["muni"] . apply( lambda s: shorten_names.munis[s] ) )
+treated["muni no space"] = (
+  treated["muni short"] . apply( lambda s:
+                                 s.replace(" ","") ) )
 treated["dept short"] = (
   treated["dept"] . apply( lambda s: shorten_names.depts[s] ) )
 
 treated["url"] = (
   treated . apply(
-    lambda row : 
-    ( "http://luiscarlosreyes.com/wp-content/uploads/2019/10/"
-      + row["dept short"] + "__" + row["muni short"]
-      + "_" + str(row["muni code"]) + ".pdf" ),
-    axis = "columns" ) .
-  replace( " ", "-" ) )
+    lambda row :
+    # "https://raw.githubusercontent.com/ofiscal/cities-output/master/reportes/" +
+    "http://luiscarlosreyes.com/wp-content/uploads/2019/10/" +
+    str(row["muni code"]) + ".pdf",
+    axis = "columns" ) )
 
+treated["title"] = "Finanzas municipales"
+treated["description"] = (
+  "¿En qué se gasta la plata el gobierno de #" +
+  treated["muni no space"] + "?" )
+
+cols_to_keep = ["muni code","muni","dept","group",
+                "url","title","description"]
 
 treated_1 = treated[ treated["group"]==1 ]
 treated_2 = treated[ treated["group"]==2 ]
@@ -51,18 +72,18 @@ treated_2 = treated[ treated["group"]==2 ]
 ( pd.concat( [ treated_1 . iloc[0:35],
                treated_2 . iloc[0:34] ],
              axis = "rows" )
-  [["muni code","muni","dept","group","url"]].
+  [cols_to_keep] .
   to_excel( "manuela.xlsx" ) )
 
 ( pd.concat( [ treated_1 . iloc[35:70],
                treated_2 . iloc[34:69] ],
              axis = "rows" )
-  [["muni code","muni","dept","group","url"]].
+  [cols_to_keep] .
   to_excel( "juan.xlsx" ) )
 
 ( pd.concat( [ treated_1 . iloc[70:],
                treated_2 . iloc[69:] ],
              axis = "rows" )
-  [["muni code","muni","dept","group","url"]].
+  [cols_to_keep] .
   to_excel( "jeff.xlsx" ) )
 
