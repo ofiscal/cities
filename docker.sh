0fa859f17179ae39d8c4c573958474a825099fd4
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-04-19 17:21:24 -0500

    Docker: mount to /mnt, not /mnt/cities
      This is how it was originally.

--- docker.sh
+++ docker.sh
@@ -1,8 +1,10 @@
 # Start a docker container based on the latest image.
-docker run --name cities -itd         \
-  -v /home/jeff/of/cities:/mnt/cities \
-  -p 7777:8888 -h 127.0.0.1           \
+docker run --name cities -itd  \
+  -v /home/jeff/of/cities:/mnt \
+  -p 7777:8888 -h 127.0.0.1    \
   ofiscal/tax.co:latest
 
 docker exec -it cities bash
-cd mnt/cities
+cd mnt
+
+docker stop cities && docker rmi cities
