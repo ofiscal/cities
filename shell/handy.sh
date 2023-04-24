904d4383580a5548cdcf39a6be501782d3ae63b8
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-04-24 17:49:55 -0500

    minor
      adjust shell snippets

--- shell/handy.sh
+++ shell/handy.sh
@@ -5,8 +5,8 @@ find . -type f \( -iname \*.py -o -iname \*.sh -o -iname \*.txt -o -iname \*.org
   # grep -i "\"pc\""
   # sed -i "s/xxx/yyy/g"
 
-make all subsample=100 > make.out 2> make.err
-make all subsample=1000
-make all subsample=100
-make all subsample=10
-make all subsample=1
+make all vintage=2019 subsample=100 > make.out 2> make.err
+make all vintage=2019 subsample=1000
+make all vintage=2019 subsample=100
+make all vintage=2019 subsample=10
+make all vintage=2019 subsample=1
