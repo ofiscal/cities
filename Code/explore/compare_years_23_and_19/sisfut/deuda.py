0f57895f2306d71e614fbe8d8a21eab3e5f079ca
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-05-09 14:48:33 -0500

    explore deudas data
      Determine that column names and dtypes do not vary
      within the two "views" (the view as of 2019 and the view as of 2023)
      of the data.

--- Code/explore/compare_years_23_and_19/sisfut/deuda.py
+++ Code/explore/compare_years_23_and_19/sisfut/deuda.py
@@ -12,26 +12,61 @@ year : GenericAlias = int
 in19 = "data/2019/sisfut/csv"
 in23 = "data/2023/sisfut/csv"
 
-# The view as of (vao) year _.
+# The "view as of" (vao) years 19 and 23.
 # This will be a dictionary, the keys of which are 2-digit years,
 # and the values of which are pandas data frames.
 vao19 : Dict [year, pd.DataFrame] = {}
 vao23 : Dict [year, pd.DataFrame] = {}
 
-vao19[13] = pd.read_csv (
-  path.join ( in19, "2013_deuda.csv" ) )
-vao23[13] = pd.read_csv (
-  path.join ( in23, "2013_deuda.csv" ) )
+for y in range(13,22):
+  vao23[y] = pd.read_csv (
+    path.join ( in23, "20" + str(y) + "_deuda.csv" ) )
+  if y <= 18:
+    vao19[y] = pd.read_csv (
+      path.join ( in19, "20" + str(y) + "_deuda.csv" ) )
 
 
 #####
 ##### Look
 #####
 
-pd.options.display.min_rows = 100
+# As of 2019, each data frame had the same columns amd dtypes
+for df in vao19.values():
+  print(df.columns
+        .equals(
+          vao19[13].columns ) )
+  print(df.dtypes
+        .equals(
+          vao19[13].dtypes ) )
 
-vao19[13].dtypes
-vao23[13].dtypes
+# As of 2023, each data frame had the same columns
+for df in vao23.values():
+  print(df.columns
+        .equals(
+          vao23[13].columns ) )
 
-vao19[13].describe().transpose()
-vao23[13].describe().transpose()
+# and they *mostly* had the same dtypes:
+pd.options.display.min_rows = 500
+for df in vao23.values():
+  print(df.dtypes
+        .equals(
+          vao23[13].dtypes ) )
+
+# The exception is at year 16, and it's no big deal --
+# it just uses ints where the others use floats.
+mismatches = pd.DataFrame()
+for c in vao23[13].columns:
+  if (vao23[13][c].dtype) != (vao23[16][c].dtype):
+    mismatches = pd.concat (
+      [ mismatches,
+        pd.Series ( { "column" : c,
+                      "2013 type" : vao23[13][c].dtype,
+                      "2016 type" : vao23[16][c].dtype } ) ],
+      axis = "columns" )
+mismatches.transpose()
+
+# The sets of columns aren't the same in the two views
+# (i.e. the view from 2019 and the view from 2023).
+( vao19[13].columns
+  . equals(
+    vao23[13].columns )
