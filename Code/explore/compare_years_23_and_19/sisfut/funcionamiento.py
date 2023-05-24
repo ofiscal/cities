07379b8ef98807858a29abd42d51f3115e7ae609
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-05-24 18:50:58 -0500

    Validate the raw funcionamiento data in the view from 2023.

--- Code/explore/compare_years_23_and_19/sisfut/funcionamiento.py
+++ Code/explore/compare_years_23_and_19/sisfut/funcionamiento.py
@@ -11,7 +11,7 @@ import Code.metadata.terms as t
 import Code.build.classify_budget_codes as codes
 
 
-(vao19,vao23) = lib.load_views_from_2019_and_2023 ( "inversion" )
+(vao19,vao23) = lib.load_views_from_2019_and_2023 ( "funcionamiento" )
 
 
 ################
@@ -27,19 +27,19 @@ for year, df in ( vao19.items() ):
 for year, df in ( vao23.items() ):
   print ( year, ": ", len ( df["Nombre Entidad"].unique() ) )
 
-# Ignoring that one, the shortest series is vao23[14].
+# Ignoring that one, the shortest series is vao23[19].
 # The others have most of the Entidades that it does.
-names14 = set ( vao23[14] ["Nombre Entidad"].unique() )
+names19 = set ( vao23[19] ["Nombre Entidad"].unique() )
 for year in range(13,21):
   names = set ( vao23[year] ["Nombre Entidad"].unique() )
   print("")
   print("year: ", year)
-  x = list(names14 - names)
+  x = list(names19 - names)
   x.sort() # stupid impure function
-  print("In 2014 and not this year.",x)
-  y = list(names - names14)
+  print("In 2019 and not this year.",x)
+  y = list(names - names19)
   y.sort() # stupid impure function
-  print("In this year and not 2014.", y)
+  print("In this year and not 2019.", y)
 
 
 ########################
@@ -67,16 +67,16 @@ for year in vao23.keys():
                       . unique() ) )
 
 # Again, they're all more or less the same, but with a handful
-# of municipalities not in 2014, and vice-versa.
-codes14 = set ( vao23[14] ["Cód. DANE Municipio"] . unique() )
+# of municipalities not in 2019, and vice-versa.
+codes19 = set ( vao23[19] ["Cód. DANE Municipio"] . unique() )
 for year in vao23.keys():
   codes = set ( vao23[year]["Cód. DANE Municipio"].unique() )
   print("")
   print("year: ", year)
-  print( "In 2014 and not this year.",
-         pd.Series(list(codes14 - codes)) . sort_values() )
-  print( "In this year and not 2014.",
-         pd.Series(list(codes - codes14)) . sort_values() )
+  print( "In 2019 and not this year.",
+         pd.Series(list(codes19 - codes)) . sort_values() )
+  print( "In this year and not 2019.",
+         pd.Series(list(codes - codes19)) . sort_values() )
 
 
 #################
@@ -89,16 +89,17 @@ for year in vao23.keys():
   print ( year, len ( vao23 [year] ["Código Concepto"]
                       . unique() ) )
 
-# Fortunately, each year has all the codes we use.
-inversion_codes_we_use = set()
-for v in codes.categs_to_codes[t.inversion].values():
-  inversion_codes_we_use = { *inversion_codes_we_use, *v }
+# PITFALL: For each year, at least one of the codes we need is absent.
+# (The worst, perhaps predictably, is 2021, which is missing 7 of them.)
+funcionamiento_codes_we_use = set()
+for v in codes.categs_to_codes[t.funcionamiento].values():
+  funcionamiento_codes_we_use = { *funcionamiento_codes_we_use, *v }
 for year in vao23.keys():
   print("")
   print(year)
-  print("Missing from this year: ",
-        inversion_codes_we_use -
-        set ( vao23 [year] ["Código Concepto"] . unique() ) )
+  print( "Missing from this year: ",
+         ( funcionamiento_codes_we_use -
+           set ( vao23 [year] ["Código Concepto"] . unique() ) ) )
 
 
 #################
@@ -114,11 +115,14 @@ for year in vao23.keys():
   df = vao23 [year]
   restricted_views_23[year] = \
     df [ df ["Código Concepto"]
-         . isin ( inversion_codes_we_use ) ]
+         . isin ( funcionamiento_codes_we_use ) ]
 
 # PITFALL: Expenses through 2016 are in thousands of pesos.
 # After that, they are in pesos.
 # (There's already code in build/ to correct for this.)
+# PITFALL: Figures for 2021 are around 5 times bigger than the previous year.
+# I suspect that's because bigger places are more likely to be
+# among those that managed to report already for 2021.
 descriptions = pd.DataFrame()
 for year in restricted_views_23.keys():
   df = restricted_views_23 [year]
@@ -127,11 +131,3 @@ for year in restricted_views_23.keys():
   descriptions = pd.concat ( [ descriptions, s ],
                              axis = 1 )
 descriptions . transpose()
-
-# PITFALL: There's exactly one negative observation
-# (among the codes of interest to us).
-for year in restricted_views_23.keys():
-  print (year)
-  df = restricted_views_23 [year]
-  print ( ( df [ df ["Obligaciones"] < 0 ] )
-          . transpose() )
