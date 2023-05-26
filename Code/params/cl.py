85c6f809e74c49f4899e1118ac1732868d771386
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-05-26 17:38:23 -0500

    PITFALL: This commit includes changes I meant to include earlier,
      specifically two commits ago,
      in e5665f85aff9840dcd9169f3efd2fa5b1c3fb46b.
      Those are the changes described by the following text:
    
        Split some code from `Code/params/cl.py`
    
          into the new file `Code/params/cl_arg_universe`,
          so that it can be imported even from the REPL
          (`cl.py` is only imported when running from the command line,
          outside of the REPL).

--- Code/params/cl.py
+++ Code/params/cl.py
@@ -1,26 +1,25 @@
 # PITFALL: Don't include this in the Makefile.
 
 import argparse, sys
+#
+import Code.params.cl_arg_universe as clau
 
-
-subsample_universe = [1,10,100,1000]
-vintage_universe   = [2019,2023]
 
 parser = argparse.ArgumentParser ()
 parser.add_argument (
   "--subsample",
   help = ( "Inverse of sample size. Should take a value in "
-           + str ( subsample_universe ) ) )
+           + str ( clau.subsample_universe ) ) )
 parser.add_argument (
   "--vintage",
   help = ( "Source year. Should take a value in "
-           + str ( vintage_universe ) ) )
+           + str ( clau.vintage_universe ) ) )
 args = parser.parse_args ()
 
 subsample = int( args.subsample )
-if not subsample in subsample_universe:
-  raise ValueError("subsample not in ", subsample_universe)
+if not subsample in clau.subsample_universe:
+  raise ValueError("subsample not in ", clau.subsample_universe)
 
 vintage   = int( args.vintage )
-if not vintage in vintage_universe:
-  raise ValueError("vintage not in ", vintage_universe)
+if not vintage in clau.vintage_universe:
+  raise ValueError("vintage not in ", clau.vintage_universe)
