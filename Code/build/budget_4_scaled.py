5874ef4d6d07fa0e60cef1bf45183026e9fa9737
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-05-30 18:20:35 -0500

    minor safety, readability edits to Code/build/budget_4_scaled.py
      The most substantial one was to generalize
    
        2014 -> Code.common.first_year
        2019 -> Code.common.last_year

--- Code/build/budget_4_scaled.py
+++ Code/build/budget_4_scaled.py
@@ -41,6 +41,7 @@ if True: # function to multiply early years by 1000
       df.loc[ df["year"] < 2017,
               c] *= 1000
     return df
+
   if True: # test it
     x = pd.DataFrame( {"a" : [0,1,2,3,4,5],
                        "b" : [0,1,2,3,4,5],
@@ -53,18 +54,23 @@ if True: # function to multiply early years by 1000
           "a" : [0,1000,2000,3000,4,5],
           "b" : [0,1000,2000,3000,4,5],
           "c" : [0,1,2,3,4,5],
-          "year" : list( range( 2013, 2019 ) ) } ) ) )
+          "year" : list ( range( 2013, 2019 ) ) } ) ) )
 
-dfs, dfs_by_muni_item = ({},{})
+dfs, dfs_by_place_item = ({},{})
 for s in ts.series:
   if True: # clean the data
     df = pd.read_csv (
       os.path.join ( source,
                      s.name + ".csv" ) )
-    df = correct_peso_columns( s.money_cols, df )
+    df = correct_peso_columns ( s.money_cols, df )
     dfs[s.name] = df
-  if True: # add percent change across years within place-item
-    df_by_muni_item = (
+
+  if True:
+    # PITFALL: This seems to assume (as happens to be true,
+    # but it would be better to ensure it in this file)
+    # that the data is sorted by year.
+    df_by_place_item = (
+      # add percent change across years within place-item
       df[["year",     "muni code", "dept code", "item categ"]
          + s.money_cols ] .
       groupby ( by = [ "muni code", "dept code", "item categ"],
@@ -72,12 +78,15 @@ for s in ts.series:
       apply( lambda df:
              lib.add_pct_change( s.money_cols[0], df) ) .
       reset_index() )
-    for year in list( range( 2014, 2019 ) ):
-      # Skip 2013 because no year precedes it,
-      # so median change is undefined.
+
+    # Test
+    for year in list (
+        range ( c.first_year + 1, # Skip the first year, because
+                                  # median change that year is undefined.
+                c.last_year + 1 ) ):
       median_change = (
-        ( df_by_muni_item
-          [df_by_muni_item["year"] == year ]
+        ( df_by_place_item
+          [df_by_place_item["year"] == year ]
           ["pct change"] ) .
         median() )
       assert median_change < 2
@@ -89,7 +98,7 @@ for s in ts.series:
         # In fact they can't be made much tighter (and still have every subsample pass).
         # Fortunately, these bounds are far more than tight enough to assure
         # the order of magnitude problem is solved.
-    dfs_by_muni_item[s.name] = df_by_muni_item
+    dfs_by_place_item[s.name] = df_by_place_item
   df.to_csv (
     os.path.join ( dest,
                    s.name + ".csv" ),
