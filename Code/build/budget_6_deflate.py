064bc75d3213bd15bdddb408ed9e9e42e6e29308
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-06-05 19:28:21 -0500

    budget_6_deflate.py
      Do away with the `dfs : Dict[str, pd.DataFrame]` object,
      which was needlessly complicated.
    
      Squash a future warning, by calling iloc[0] on a single-element series
      before converting it to a float.
    
      Add some tests to ensure the deflation makes sense.

--- Code/build/budget_6_deflate.py
+++ Code/build/budget_6_deflate.py
@@ -11,32 +11,52 @@ if True: # folders
                           "recip-" + str(c.subsample) )
   dest   = os.path.join ( c.outdata, "budget_6_deflate",
                           "recip-" + str(c.subsample) )
-  if not os.path.exists( dest ):
-    os.makedirs(         dest )
+  if not os.path.exists ( dest ):
+    os.makedirs         ( dest )
 
-if True: # input data
-  dfs = {}
-  for s in ser.series:
-    dfs[s.name] = pd.read_csv (
-      os.path.join ( source,
-                     s.name + ".csv" ) )
+if True: # The inflation data.
   deflator = pd.read_csv (
     os.path.join ( c.outdata,
                    "inflation.csv" ) )
-  deflator["deflator"] = ( # normalize in terms of 2018 pesos
+  deflator["deflator"] = ( # This statement has two purposes:
+    # (1) Normalize it to be in terms of 2018 pesos.
+    # (2) Invert it, so that below I can multiply a series by it,
+    #     which is faster than dividing the series by it.
     1 / ( deflator["deflator"] /
-          float( deflator
-                 [ deflator["year"] == 2018 ]
-                 ["deflator"] ) ) )
+          float ( deflator
+                  [ deflator["year"] == 2018 ]
+                  ["deflator"]
+                  . iloc[0] ) ) )
 
 for s in ser.series:
-  df = dfs[s.name]
-  df = df.merge( deflator, on = "year" )
+  df0 : pd.DataFrame = ( # The original, not to be mutated.
+    pd.read_csv ( os.path.join ( source,
+                                 s.name + ".csv" ) ) )
+  df1 : pd.DataFrame = (
+    df0 . copy () # A copy, to be mutated and saved.
+    . merge ( deflator, on = "year" ) )
+
   for c in s.money_cols:
-    df[c] = df[c] * df["deflator"]
-  df = df.drop( columns = ["deflator"] )
-  dfs[s.name] = df
-  df.to_csv (
+    df1[c] = df1[c] * df1["deflator"]
+    if True: # Verify (loosely) that it worked --
+             # i.e. that the ratio total money from 2018 to 2013
+             # has fallen by between 1.2 and 1.3.
+             # (The peso devalued by a factor of very close to 1.25
+             # over that time. The exact value depends a bit
+             # on which inflation series (from 2023 or from 2019) is used.)
+      ratio_pre_deflation  = ( ( df0 [ df0["year"] == 2018 ] [c] . sum() ) /
+                               ( df0 [ df0["year"] == 2013 ] [c] . sum() ) )
+      ratio_post_deflation = ( ( df1 [ df1["year"] == 2018 ] [c] . sum() ) /
+                               ( df1 [ df1["year"] == 2013 ] [c] . sum() ) )
+      assert ratio_pre_deflation > ratio_post_deflation * 1.2
+      assert ratio_pre_deflation < ratio_post_deflation * 1.3
+      assert ( # Also, it should be that the 2013 peso data is bigger
+               # in the new data frame than in the old one.
+        ( df0 [ df0["year"] == 2013 ] [c] . sum() ) <
+        ( df1 [ df1["year"] == 2013 ] [c] . sum() ) )
+
+  df1 = df1.drop( columns = ["deflator"] )
+  df1.to_csv (
     os.path.join ( dest,
                    s.name + ".csv" ),
     index = False )
