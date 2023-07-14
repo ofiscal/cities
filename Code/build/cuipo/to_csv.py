52a9b8008515be42d3f91854e7eb4c8f757970ca
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-07-14 15:38:00 -0500

    code to convert original cuipo data from .xlsx to .csv

---
+++ Code/build/cuipo/to_csv.py
@@ -0,0 +1,36 @@
+# CUIPO provides data in .xlsx format.
+# This converts that data to .csv format.
+
+import pandas as pd
+
+
+### Convert gastos from xlsx to csv
+
+g22 = pd.read_excel (
+  path.join ( "data/cuipo/",
+              "Ejecucion_GastosDic2022.xlsx" ) )
+
+g22.to_csv (
+  path.join ( "data/cuipo/",
+              "Ejecucion_GastosDic2022.csv" ),
+  index = False )
+
+g22_2 = pd.read_csv (
+  path.join ( "data/cuipo/",
+              "Ejecucion_GastosDic2022.csv" ) )
+
+
+### Convert gastos from xlsx to csv
+
+i22 = pd.read_excel (
+  path.join ( "data/cuipo/",
+              "Ejecucion_IngresosDic2022.xlsx" ) )
+
+i22.to_csv (
+  path.join ( "data/cuipo/",
+              "Ejecucion_IngresosDic2022.csv" ),
+  index = False )
+
+i22_2 = pd.read_csv (
+  path.join ( "data/cuipo/",
+              "Ejecucion_IngresosDic2022.csv" ) )
