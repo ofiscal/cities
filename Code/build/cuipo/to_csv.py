e48951de9d1753013f614f72a67e6dc590927db2
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-07-14 15:53:14 -0500

    add a (manual) test

--- Code/build/cuipo/to_csv.py
+++ Code/build/cuipo/to_csv.py
@@ -16,10 +16,6 @@ g22.to_csv (
               "Ejecucion_GastosDic2022.csv" ),
   index = False )
 
-#g22_2 = pd.read_csv (
-#  path.join ( "data/cuipo/2022",
-#              "Ejecucion_GastosDic2022.csv" ) )
-
 
 ### Convert gastos from xlsx to csv
 
@@ -32,6 +28,20 @@ i22.to_csv (
               "Ejecucion_IngresosDic2022.csv" ),
   index = False )
 
-#i22_2 = pd.read_csv (
-#  path.join ( "data/cuipo/2022",
-#              "Ejecucion_IngresosDic2022.csv" ) )
+if False: # For interactive testing
+  # Almost everything is equal, i.e. unchanged by the formatting.
+  # The one field that is an exception, "22_BPIN" in "gastos",
+  # is irrelevant to this analysis. (It's a type casting glitch,
+  # not a data corruption one.)
+  res = {}
+  for excel, slang_name, filename in [
+      (g22, "gastos",   "Ejecucion_GastosDic2022"),
+      (i22, "ingresos", "Ejecucion_IngresosDic2022") ]:
+    csv = pd.read_csv (
+      path.join ( "data/cuipo/2022",
+                  filename + ".csv" ) )
+    res[slang_name] = pd.Series (
+      { c : ( excel[c]
+              . equals ( csv[c] ) )
+        for c in set ( list(excel.columns) +
+                       list(csv.columns) ) } )
